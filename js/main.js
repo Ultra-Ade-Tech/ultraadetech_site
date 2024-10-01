cb02c0dea14c901e3f86b8e4b572e15d76e0d347
jrshittu <misterjrshittu@gmail.com>
2024-10-01 02:07:31 +0100

    add fb embeds

--- js/main.js
+++ js/main.js
@@ -119,4 +119,23 @@
         }
     });
 
+    $(document).ready(function(){
+        $('.social-feeds-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
+    });
+
 })(jQuery);
